e929d16cde05823bf4fd37991cdd5be87b11e52f
MrrFahrenheit <226563580+MrrFahrenheit@users.noreply.github.com>
2026-09-24 12:57:43 -0600

    Backend Enpoint ENV variable Set

--- frontend/lib/api-client.ts
+++ frontend/lib/api-client.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: process.env.BACKEND_URL ,
   headers: {
     'Content-Type': 'application/json',
   },
